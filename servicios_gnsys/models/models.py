a214ee5ba4941b9bb024f6f2995a51e7be615930
Cesar <kliko1989@gmail.com>
2021-03-01 11:39:28 -0600

    swqwsqw

--- servicios_gnsys/models/models.py
+++ servicios_gnsys/models/models.py
@@ -167,6 +167,7 @@ class productos_en_servicios(models.Model):
 class equipo_series(models.Model):
     _inherit = 'stock.production.lot'
     servicio = fields.Many2one('servicios', string="Servicio serie")
+    x_studio_impresiones=fields.Integer()
 
 
 
