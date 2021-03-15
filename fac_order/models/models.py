cbc9e4bde7b026a13f71a994d4f29fd5fb324d6f
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-03-15 17:31:06 -0600

    Update models.py

--- fac_order/models/models.py
+++ fac_order/models/models.py
@@ -24,7 +24,7 @@ class fac_order(models.Model):
       nameDos = fields.Char()
       month = fields.Selection(valores,string='Mes',default='04')
       #year = fields.Selection(''.join(get_years()), string='Año',default=2020)
-      year = fields.Selection([['2010', '2036']], string='Año',default=2020)
+      year = fields.Selection([['2010', '2036']], string='Año',default=2010)
       excedente=fields.Text(string='Excedentes')
                              
      
