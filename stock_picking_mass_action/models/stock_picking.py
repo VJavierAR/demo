0ae850cfe88d5a8e0bf3e76b8498b6a0583c3b6a
Cesar <kliko1989@gmail.com>
2021-03-22 14:06:46 -0600

    xsaxas

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -41,7 +41,7 @@ class StockPicking(Model):
     x_studio_toneres=fields.Char(compute='proHtml')
     x_studio_backorder=fields.Boolean()
     x_studio_tecnico = fields.Many2one('hr.employee','Tecnico')
-
+    x_studio_ticket=fields.Char(related='sale_id.origin')
 
     @api.depends('move_ids_without_package')
     def proHtml(self):
