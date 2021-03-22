f9e8a0edc4ec84f5c749054dc4779569beace9c0
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-03-22 16:16:21 -0600

    Update __manifest__.py

--- helpdesk_update/__manifest__.py
+++ helpdesk_update/__manifest__.py
@@ -27,8 +27,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/helpdesk_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'wizard/wizard.xml'
