66e466456b27a38295258cddaa7afa2a7f2d615b
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-03-24 12:38:43 -0600

    Update __manifest__.py

--- contadores/__manifest__.py
+++ contadores/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','portal','stock', 'x_studio_historico_de_componentes'],
+    'depends': ['base','portal','stock', 'helpdesk_update'],
 
     # always loaded
     'data': [
