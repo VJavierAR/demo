87e48882c67a19b0c5b2ae3e77936607f6fa3255
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-03-30 14:17:36 -0600

    Update models.py

--- product_product/models/models.py
+++ product_product/models/models.py
@@ -11,9 +11,11 @@ _logger = logging.getLogger(__name__)
 class DcasUpdate(models.Model):
 	_inherit = 'product.product'
 	x_studio_color_bn = fields.Selection([('B/N','B/N'),('Color','Color')], string = 'Color - B/N', store = True)
+	x_studio_toner_compatible = fields.Many2many('product.product', string = "Toner compatible")
 
 class ProductTemplate(models.Model):
 	_inherit = 'product.template'
 	x_studio_color_bn = fields.Selection([('B/N','B/N'),('Color','Color')], string = 'Color - B/N', store = True)
+	x_studio_toner_compatible = fields.Many2many('product.product', string = "Toner compatible")
 
      
