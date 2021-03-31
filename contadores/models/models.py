b64099c032adeb22005a17b0994fa76bdb35703a
Marco <47366939+VJavierAR@users.noreply.github.com>
2021-03-31 14:09:57 -0600

    Update models.py

--- contadores/models/models.py
+++ contadores/models/models.py
@@ -143,7 +143,7 @@ class dcas(models.Model):
         compatibles_list = []
         productos = self.sudo().env['product.product'].search([])
         #productos = productos.sudo().filtered(lambda x:  x.categ_id.id == 5 and x.x_studio_color == 'Negro' and self.x_studio_field_qYMJD in x.x_studio_toner_compatible.ids)
-        productos = productos.filtered(lambda x:  x.categ_id.id == 5)
+        productos = productos.filtered(lambda x:  x.categ_id.name.lower() == 'toner')
         _logger.info('productos' + str(productos))
         if productos:
             domain = [('id', 'in', productos.ids)]
